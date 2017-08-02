9be256dac81e967cc3d0469725580e7313319b2f
Zakaria Mahmud <zaka.pong@yahoo.com>
2017-08-02 11:25:23 +0200

    adding angular client

--- route.js
+++ route.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
+const Contact = require('./models/contacts');
+
 //retrieving contact
 router.get('/contacts', (req, res, next) => {
-    res.send('Retrieving the contact list');
+    Contact.find(function(err, contacts) {
+        res.json(contacts)
+    });
 });
 
 // add contact
 router.post('/contacts', (req, res, next) => {
-    res.send('Posting the contact list');
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+
+    });
+
+    newContact.save((err, contact) => {
+        if (err) {
+            res.json({
+                msg: 'Failed to add contact'
+            });
+        } else {
+            res.json({
+                msg: 'Contact added sucessfully'
+            });
+        }
+    });
 });
 
 //delete contacts
-router.delete('/contacts', (req, res, next) => {
-    res.send('Deleting the contact list');
+router.delete('/contacts/:id', (req, res, next) => {
+    Contact.remove({
+        _id: req.params.id
+    }, function(err, result) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 module.exports = router;
